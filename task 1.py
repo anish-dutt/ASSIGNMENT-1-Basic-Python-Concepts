aed6b2d41a6a2112b0ef30cdc1ac06d95a73dee3
anish-dutt <anishd1112@gmail.com>
2026-01-11 15:35:18 +0530

    Add files via upload

---
+++ task 1.py
@@ -0,0 +1,8 @@
+a = float(input("Enter the first Number: "))
+b = float(input("Enter the second Number: "))
+
+
+print ("Addition: ",int(a+b))
+print ("Subtraction: ",int(a-b))
+print ("Multiplication: ",int(a*b))
+print ("Division: ",round(int((a/b)),2))
---
+++ task 2.py
@@ -0,0 +1,3 @@
+n1= input("Enter your first name: ")
+n2= input("Enter your last name: ")
+print( "Hello,",str(n1+(" "+n2)),"! Welcome to Python program.")
